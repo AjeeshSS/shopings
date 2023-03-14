82d8eb802b181db4f209969c94e99116564bb3ea
AjeeshSS <ajeeshss2019@gmail.com>
2023-03-14 18:00:15 +0530

    added static_dirs

--- shopings/settings.py
+++ shopings/settings.py
@@ -133,5 +133,9 @@ if DEBUG:
 else:
     STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
+
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+    ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
