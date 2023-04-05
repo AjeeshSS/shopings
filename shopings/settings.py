bb83b633925818d1b70ceec3e16f8230b9251951
AjeeshSS <ajeeshss2019@gmail.com>
2023-04-05 17:47:12 +0530

    bug fixed

--- shopings/settings.py
+++ shopings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1lang6(7$k0*9a!4g$zblust$7)_q34$7aoj%e9)oj(6%el25@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
